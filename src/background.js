1a55ce1c6822e8ca1b7932067d1d3ca8327ad78f
Guy Eden <edenidan@gmail.com>
2021-03-07 16:04:48 +0200

    itkapalty

--- src/background.js
+++ src/background.js
@@ -1,4 +1,8 @@
 //on message from content script, convert urls to html (by fetching them)
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
 chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
     result = {};
     promises = []
@@ -9,11 +13,21 @@ chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
         p.then((html)=>result[siteT] = html);
         promises.push(p);
     }
+    
+    Promise.all(promises).then(() =>{
+        sendResponse(result);
+    });
 
-    sendResponse(new Promise((resolve,reject)=>{
-        Promise.all(promises).then(() =>{
-            resolve(result);
-        });
-    }));
+    await sleep(1000)
+
+    // sendResponse(new Promise((resolve,reject)=>{
+    //     Promise.all(promises).then(() =>{
+    //         console.log(+ new Date())
+    //         console.log(result)
+    //         resolve(result);
+    //     });
+
+        //resolves with empty object? check
+    //}));
     
 })
--- src/content.js
+++ src/content.js
@@ -2,17 +2,14 @@
 var listURL = chrome.runtime.getURL("data/list.db");
 
 $(document).ready(() => 
-    $.get(listURL).then(data => analize(data))
+    $.get(listURL).then(data => analize(JSON.parse(data)))
 )
 
 
 function analize(data){
 
-    chrome.runtime.sendMessage(JSON.parse(data),(response)=>{
-        console.log(response)
-        console.log(typeof response)
-        response.then((result)=>console.log(result));
-    })
+
+
     //send urls as a message to the background script, get the matching htmls as a response
 
     //find an html with a small diff with the current page
