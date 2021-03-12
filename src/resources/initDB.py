459120c41469d56325dc3c2af0e796ce05f70e5b
Guy Eden <edenidan@gmail.com>
2021-03-12 02:07:04 +0200

    moved initDB into resources

--- src/resources/initDB.py
+++ src/resources/initDB.py
@@ -1,16 +1,17 @@
 import base64
 import os
 import json
+dataFolder = r"data/"
 
 result = {}
-for fileName in filter(lambda f: f.endswith('.html'), os.listdir('data/')):
+for fileName in filter(lambda f: f.endswith('.html'), os.listdir(dataFolder)):
     
-    filePath = 'data/' + fileName
+    filePath = os.path.join(dataFolder,fileName)
     with open(filePath,encoding='utf-8') as f:
         html = f.read()
     encoded = base64.b64encode(html.encode()).decode()
     
     result[os.path.splitext(fileName)[0]] = encoded
 
-with open(r'data/result.json', 'w') as fp:
+with open(os.path.join(dataFolder,'result.json'), 'w') as fp:
     json.dump(result, fp)
