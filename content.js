b94b87a707f83a8ba8dfa20a008baf5caffd65b6
Guy Eden <edenidan@gmail.com>
2021-03-14 03:07:55 +0200

    created executeBackground function

--- content.js
+++ content.js
@@ -1,41 +1,36 @@
 
 notified = false;
-function NotifyUser(message){
-    if(notified) return;
-    notified=true;
+function NotifyUser(message) {
+    if (notified) return;
+    notified = true;
 
     alert(message);
 }
 
-function checkDomain(domain){
-
-    chrome.runtime.sendMessage({
-        op:"checkDomain",
-        params:{domain: domain}}
-        , response => {
-        if(response)
-            NotifyUser('The current website have been reported as a DANGEROUS one, beware.');
-      });
+function executeBackground(op, params, sendResponse) {
+    chrome.runtime.sendMessage({ op: op, params: params }, sendResponse);
 }
 
-function checkHTMLDiff(){
+function checkDomain(domain) {
+    executeBackground('checkDomain',{domain:domain}, response => {
+            if (response)
+                NotifyUser('The current website have been reported as a DANGEROUS one, beware.');
+        });
+}
+
+function checkHTMLDiff() {
     var listURL = chrome.runtime.getURL("data/result.json");
-    $(document).ready(() => 
+    $(document).ready(() =>
         $.get(listURL).then(data => analize(data))
     )
 }
 
-function analize(data){
+function analize(data) {
     currentHTML = document.documentElement.innerHTML;
-
-    chrome.runtime.sendMessage({
-        op:"checkHTMLDiff",
-        params:{currentHTML:currentHTML ,data: data}
-    }, response => {
-        if(response)
+    executeBackground('checkHTMLDiff',{ currentHTML: currentHTML, data: data }, response => {
+        if (response)
             NotifyUser(response);
-      });
-
+    });
 }
 
 const currentURL = window.location.href;
